81d0fba6674b72f2bece114c36b76430be852a86
theroos <roosonline7@gmail.com>
2025-07-18 02:19:40 +0530

    Android app using Supabase database. Now much more progress done. The data part is complete. Now we moved on to storage part.Currently working on the image showing part.

--- app/src/main/java/com/example/supabaseappnew/StorageActivity.kt
+++ app/src/main/java/com/example/supabaseappnew/StorageActivity.kt
@@ -34,35 +34,48 @@ class StorageActivity : AppCompatActivity() {
 
         storageRecyclerView = findViewById(R.id.media_recyclerview)
 
-         supabase1 = createSupabaseClient(
+        supabase1 = createSupabaseClient(
             supabaseUrl = "https://aaukmjyjnmgsbgrtwzho.supabase.co",
             supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhdWttanlqbm1nc2JncnR3emhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY2OTY4MTMsImV4cCI6MjA1MjI3MjgxM30.R6Y-P3L2LAwf9UtmolyRI2tUfwO0zpQvlT6HARO8r6Y"
         )
         {
-            install(io.github.jan.supabase.storage.Storage)
+            install(Storage)
             install(Postgrest)
         }
-        
-        fetchImages()
 
+        fetchImages()
 
 
     }
 
-    private fun fetchImages(){
+    private fun fetchImages() {
         lifecycleScope.launch {
-            try{
+            try {
                 val imagelist = supabase1.storage.from("images").list()
+
+                imagelist.forEach{
+                    Log.d("SupabaseFetch", "Total items: ${imagelist.size}")
+                    Toast.makeText(this@StorageActivity, "Total items: ${imagelist.size}", Toast.LENGTH_SHORT).show()
+                }
+
                 val urls = imagelist.map { supabase1.storage.from("images").publicUrl(it.name)}
                 storageRecyclerView.layoutManager = GridLayoutManager(this@StorageActivity, 2)
                 storageRecyclerView.adapter = ImageAdapter(urls)
-                Toast.makeText(this@StorageActivity, "Adapter set with ${urls.size} images", Toast.LENGTH_SHORT).show()
-                //Log.d("AdapterCheck", "Adapter set with ${urls.size} images")
-                Toast.makeText(this@StorageActivity, "Fetched: ${imagelist.size}", Toast.LENGTH_SHORT).show()
-                //Log.d("ImageList", "Fetched: ${imagelist.size}")
-            } catch (e: Exception){
-                Toast.makeText(this@StorageActivity, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@StorageActivity,
+                    "Adapter set with ${urls.size} images",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Toast.makeText(
+                    this@StorageActivity,
+                    "Fetched: ${imagelist.size}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } catch (e: Exception) {
+                Toast.makeText(this@StorageActivity, "Error: ${e.message}", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
     }
 }
+
