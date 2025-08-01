aa2f2a0a180a9007231a6ef20ef148b0ce1fefe5
theroos <roosonline7@gmail.com>
2025-08-02 02:51:00 +0530

    Android app using Supabase database. Now much more progress done. Currently working on the image showing part. Currently working on photo delete.

--- app/src/main/java/com/example/supabaseappnew/FullImagePagerAdapter.kt
+++ app/src/main/java/com/example/supabaseappnew/FullImagePagerAdapter.kt
@@ -6,8 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class FullImagePagerAdapter(private val imageUrls: List<String>): RecyclerView.Adapter<FullImagePagerAdapter.PagerViewHolder>() {
 
@@ -33,8 +38,29 @@ class FullImagePagerAdapter(private val imageUrls: List<String>): RecyclerView.A
             .into(holder.fullimageview)
 
         holder.deleteimagebtn.setOnClickListener {
+            AlertDialog.Builder(holder.itemView.context).setTitle("Delete this image permanently?").setMessage("This action cannot be undone").setPositiveButton("Delete")
+            {_, _ ->
+                val url = imageUrls[position]
+                val path = getFilePathFromUrl(url)
 
+                CoroutineScope(Dispatchers.IO).launch{
+                    val success = deleteImageFromSupabase(path)
+                    withContext(Dispatchers.Main){
+                        if(success){
+                            Toast.makeText(holder.itemView.context, "Image deleted successfully", Toast.LENGTH_SHORT).show()
+                        } else{
+                            Toast.makeText(holder.itemView.context, "Failed to delete image", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+                .setNegativeButton("Cancel", null).show()
         }
+    }
+
+    private fun getFilePathFromUrl(url: String): String {
+        val baseUrl = "https://aaukmjyjnmgsbgrtwzho.supabase.co/storage/v1/object/public/images/"
+        return url.removePrefix(baseUrl)
     }
 
     override fun getItemCount(): Int {
